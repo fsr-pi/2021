eb85f3bdc6f289e0e0a7b1c56429d7978beb8789
Boris Milasinovic <boris.milasinovic@fer.hr>
2021-05-13 19:19:28 +0200

    PI06 - Rad s bazom podataka

---
+++ Primjeri/DataAccess/DataReader/Program.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+
+namespace DataReader
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      ShowProducts();
+      Console.WriteLine();      
+      ShowProductsBetterVersion();     
+    }
+
+    private static void ShowProducts()
+    {
+      string connString = "Data Source=rppp.fer.hr,3000;Initial Catalog=Firma;User Id=rppp;Password=do-not-put-password-in-source-code";
+
+      IDbConnection conn = new SqlConnection(connString);
+
+      IDbCommand command = new SqlCommand();
+      command.CommandText = "SELECT TOP 3 * FROM Artikl";
+      command.Connection = conn;
+
+      conn.Open();
+
+      IDataReader reader = command.ExecuteReader();
+      while (reader.Read())
+      {
+        object productCode = reader["SifArtikla"];
+        object productName = reader["NazArtikla"];
+        object isService = reader["ZastUsluga"];                
+        Console.WriteLine("{0} - {1}  {2}", productCode.ToString(), productName.ToString(), (bool)isService ? "(Service)" : "");
+      }
+      reader.Close();
+
+      conn.Close();
+    }    
+
+    private static void ShowProductsBetterVersion()
+    {
+      string connString = GetConnectionString();
+      try
+      {
+        using (var conn = new SqlConnection(connString))
+        {
+          using (var command = conn.CreateCommand())
+          {
+            command.CommandText = "SELECT TOP 3 * FROM Artikl";
+            command.Connection = conn;
+
+            conn.Open();
+
+            using (var reader = command.ExecuteReader())
+            {
+              while (reader.Read())
+              {
+                object productCode = reader["SifArtikla"];
+                object productName = reader["NazArtikla"];
+                object isService = reader["ZastUsluga"];
+                Console.WriteLine("{0} - {1}  {2}", productCode.ToString(), productName.ToString(), (bool)isService ? "(Service)" : "");
+              }
+            }
+          }
+        }
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine("Pogreška: " + exc.Message + exc.StackTrace);
+      }
+    }
+
+    private static string GetConnectionString()
+    {            
+      var configBuilder = new ConfigurationBuilder()                              
+                              .AddJsonFile("appsettings.json");
+
+      #region Variant with environment name and secret file                              
+      var environmentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
+      configBuilder = configBuilder.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+
+      bool useSecretFile = "Development".Equals(environmentName, StringComparison.OrdinalIgnoreCase);
+      if (useSecretFile)
+      {
+        configBuilder = configBuilder.AddUserSecrets("Firma");
+      }
+      #endregion
+
+      var config = configBuilder.Build();
+
+      //string connString = config["ConnectionStrings:Firma"];
+      string connString = config.GetConnectionString("Firma");
+
+      #region if using secret file...
+      if (useSecretFile)
+      {
+        connString = connString.Replace("sifra", config["FirmaSqlPassword"]);
+      }
+      #endregion
+
+      return connString;
+    }
+  }
+}
---
+++ Primjeri/DataAccess/ParamsAndProcs/Program.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.Common;
+
+namespace ParamsAndProcs
+{
+  public class Program
+  {
+    static IConfigurationRoot config;
+    static DbProviderFactory factory = SqlClientFactory.Instance;
+    //static DbProviderFactory factory = DbProviderFactories.GetFactory("Microsoft.Data.SqlClient"); //nije dio .Net Core, potrebno eksplicitno ranije registrirati  s   DbProviderFactories.RegisterFactory(factoryName, MySQLProviderFactory.Instance), npr. DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance); (jednom, u glavnom programu ili sl.)
+
+    public static void Main(string[] args)
+    {
+      config = new ConfigurationBuilder()
+                  //.SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json")
+                  .Build();
+
+      ParametrizedQueryDemo();
+      Console.ReadLine();
+      decimal threshold = decimal.Parse(config["PriceThreshold"]);
+      ProcedureDemo(threshold);
+    }
+
+    /// <summary>
+    /// Primjer stvaranja priključka na bazu korištenjem razreda DBProviderFactory
+    /// Upit se postavlja korištenjem parametara i vraća 2 skupa rezultata
+    /// </summary>
+    private static void ParametrizedQueryDemo()
+    {     
+      using (DbConnection conn = factory.CreateConnection())
+      {
+        conn.ConnectionString = config.GetConnectionString("Firma");
+        using (DbCommand command = factory.CreateCommand())
+        {
+          command.Connection = conn;
+          command.CommandText = "SELECT TOP 3 * FROM Artikl WHERE JedMjere = @JedMjere ORDER BY CijArtikla DESC;" +
+            "SELECT TOP 3 * FROM Artikl WHERE JedMjere = @JedMjere AND CijArtikla > @Cijena ORDER BY CijArtikla";
+          DbParameter param = command.CreateParameter();
+          param.ParameterName = "JedMjere";
+          param.DbType = System.Data.DbType.String;
+          param.Value = "kom";
+          command.Parameters.Add(param);
+
+          param = factory.CreateParameter();
+          param.ParameterName = "Cijena";
+          param.DbType = System.Data.DbType.Decimal;
+          param.Value = 100m;
+          command.Parameters.Add(param);
+          conn.Open();
+          using (DbDataReader reader = command.ExecuteReader())
+          {
+            bool firstResult = true;
+            do
+            {
+              Console.WriteLine(firstResult ? "The most expensive products sold in pieces:" : "The cheapest products sold in pieces");
+              Console.WriteLine("-----------------------------");
+              while (reader.Read())
+              {
+                Console.WriteLine("\t{0} - {1}", reader["NazArtikla"], reader["CijArtikla"]);
+              }
+              Console.WriteLine();
+              firstResult = false;
+            }
+            while (reader.NextResult());
+          }
+        }
+      }
+    }
+
+    private static void ProcedureDemo(decimal priceThreshold)
+    {      
+      using (DbConnection conn = factory.CreateConnection())
+      {
+        conn.ConnectionString = config.GetConnectionString("Firma");
+        using (DbCommand command = factory.CreateCommand())
+        {
+          command.Connection = conn;
+          command.CommandText = "ap_ArtikliSkupljiOd";
+          command.CommandType = System.Data.CommandType.StoredProcedure;
+
+          DbParameter param = command.CreateParameter();
+          param.ParameterName = "Prag";
+          param.DbType = System.Data.DbType.Decimal;
+          param.Value = priceThreshold;
+          command.Parameters.Add(param);
+
+          param = command.CreateParameter();
+          param.ParameterName = "BrojSkupljih";
+          param.DbType = System.Data.DbType.Int32;
+          param.Direction = System.Data.ParameterDirection.Output;
+
+          command.Parameters.Add(param);
+
+          param = command.CreateParameter();
+          param.ParameterName = "BrojJeftinijih";
+          param.DbType = System.Data.DbType.Int32;
+          param.Direction = System.Data.ParameterDirection.Output;
+          command.Parameters.Add(param);
+
+          conn.Open();
+          using (DbDataReader reader = command.ExecuteReader())
+          {
+            Console.WriteLine("Products with the price above the threashold ({0})", priceThreshold);
+            Console.WriteLine("---------------------------------");
+            while (reader.Read())
+            {
+              Console.WriteLine("\t{0} - {1}", reader["NazArtikla"], reader["CijArtikla"]);
+            }
+            Console.WriteLine("---------");
+          }
+          //we are finished with reader -> read parameters vakues
+          Console.WriteLine("Above threashold: {0}, Below threashold: {1}",
+              command.Parameters["BrojSkupljih"].Value, 
+              command.Parameters["BrojJeftinijih"].Value);
+        }
+      }
+    }
+  }
+}
